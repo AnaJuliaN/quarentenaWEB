41310dfbf72ba8228cd1d2621a5ff0699c9f7b0f
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2020-05-20 20:30:02 -0300

    Exercicio de Querentena

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Teste</title>
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+    <h1>Página com links dos meus exercícios de quarentena</h1>
+    <br />
+    <a href='exequarentena.php'>20200519 - Exercicio de Quarentena</a>
+</body>
+</html>
